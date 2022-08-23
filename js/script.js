2d212fc810e6ffeb0b281bc0b06c9a360a2955a4
Will <wmchance@gmail.com>
2022-08-23 15:47:38 +0700

    'T-shirt Info' Section updated

--- js/script.js
+++ js/script.js
@@ -15,3 +15,28 @@ jobRoleField.addEventListener ('change', (e) => {
     }
 
 })
+
+//'T-shirt info' section 
+const designSelect = document.getElementById('design');
+const colorSelect = document.getElementById('color');
+const colorOptions = colorSelect.children;
+
+colorSelect.disabled = true; //prevent user from selecting color prior to selecting a 'design'
+
+designSelect.addEventListener('change', e => {
+    colorSelect.disabled = false;
+
+    for (let i=1; i<colorOptions.length; i++) {
+        const designSelectValue = e.target.value;
+        const optionThemes = colorOptions[i].getAttribute('data-theme');
+        colorOptions[i].hidden = true;
+        colorOptions[i].selected = false;
+        //console.log(designSelectValue);
+        //console.log(optionThemes); 
+
+        if (designSelectValue === optionThemes) {
+            colorOptions[i].hidden = false;
+            colorOptions[i].selected = true; 
+        }
+    }
+})
